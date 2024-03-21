c0cd6285e96a871517a41db4a372ee9ae8fb2aa3
keeo <pablorizosanchez@gmail.com>
2024-03-21 19:27:56 +0100

    refactors

--- lib/widgets/card_details/price_info.dart
+++ lib/widgets/card_details/price_info.dart
@@ -8,66 +8,72 @@ import '../../models/price_details.dart';
 
 class PriceInfoDetails extends StatelessWidget {
   final CardModel card;
+
   PriceInfoDetails({super.key, required this.card});
+
   final PriceController priceController = Get.find<PriceController>();
 
   @override
   Widget build(BuildContext context) {
-
-    PriceDetails priceDetails = priceController.state.priceDetails.value.entries
-        .firstWhere((generationEntry) => generationEntry.key == card.setNum)
-        .value
-        .entries
-        .firstWhere((cardEntry) => cardEntry.key == card.cardNum)
-        .value;
-      return Positioned(
-        top: 0,
-        right: 0,
-        child: Container(
-          decoration: const BoxDecoration(
-            borderRadius: BorderRadius.only(
-              bottomLeft: Radius.circular(8),
-              topRight: Radius.circular(8),
-            ),
-            color: Colors.black,
+    return Positioned(
+      top: 0,
+      right: 0,
+      child: Container(
+        decoration: const BoxDecoration(
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(8),
+            topRight: Radius.circular(8),
           ),
-          child: Padding(
-              padding:
-              const EdgeInsets.only(bottom: 8, left: 8, top: 10, right: 10),
-              child: Row(
-                children: [
-                  SizedBox(height: 18, child: Image.asset(Constants.cardTrader)),
-                  const SizedBox(
-                    width: 5,
-                  ),
-                  priceDetails.priceCents != null
-                      ? Row(
-                    children: [
-                      Text(
-                        priceDetails.priceCents!.toStringAsFixed(2).replaceAll(".", ","),
-                        style: (Constants.cabinStyle.copyWith(
-                            color: Colors.white60,
-                            fontSize: 15,
-                            fontWeight: FontWeight.w800,
-                            letterSpacing: 0.7)),
-                      ),
-                      Text(
-                        " ${priceDetails.currency} ",
-                        style: (Constants.cabinStyle
-                            .copyWith(color: Colors.white60, fontSize: 13)),
-                      )
-                    ],
-                  )
-                      : const SizedBox(
-                    width: 43,
-                    child: LinearProgressIndicator(
-                      color: Colors.white10,
-                    ),
-                  ),
-                ],
-              )),
+          color: Colors.black,
         ),
-      );
-
+        child: Padding(
+            padding:
+                const EdgeInsets.only(bottom: 8, left: 8, top: 10, right: 10),
+            child: Row(
+              children: [
+                SizedBox(height: 18, child: Image.asset(Constants.cardTrader)),
+                const SizedBox(
+                  width: 5,
+                ),
+                Obx(() {
+                  PriceDetails priceDetails = priceController
+                      .state.priceDetails.value.entries
+                      .firstWhere((generationEntry) =>
+                          generationEntry.key == card.setNum)
+                      .value
+                      .entries
+                      .firstWhere((cardEntry) => cardEntry.key == card.cardNum)
+                      .value;
+                  return priceDetails.priceCents != null
+                      ? Row(
+                          children: [
+                            Text(
+                              priceDetails.priceCents!
+                                  .toStringAsFixed(2)
+                                  .replaceAll(".", ","),
+                              style: (Constants.cabinStyle.copyWith(
+                                  color: Colors.white60,
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.w800,
+                                  letterSpacing: 0.7)),
+                            ),
+                            Text(
+                              " ${priceDetails.currency} ",
+                              style: (Constants.cabinStyle.copyWith(
+                                  color: Colors.white60, fontSize: 13)),
+                            )
+                          ],
+                        )
+                      : const SizedBox(
+                          width: 43,
+                          child: LinearProgressIndicator(
+                            color: Colors.white10,
+                          ),
+                        );
+                })
+              ],
+            )),
+      ),
+    );
   }
 }
